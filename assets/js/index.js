e3c686dd45466c10a26d3f36fe5f103babab80f1
andrewsrealestate <team@andrewsrealestate.com>
2024-11-01 08:34:52 -0700

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -407,72 +407,65 @@ function getMonthlyCounts(data) {
 
   // chart 5
 
-  var options = {
+var options = {
     series: [{
-      name: "Net Sales",
-      data: [4, 10, 25, 12, 25, 18, 40, 22, 7]
+        name: "Net Sales",
+        data: [4, 10, 25, 12, 25, 18, 40, 22, 7]
     }],
     chart: {
-      //width:150,
-      height: 115,
-      type: 'area',
-      sparkline: {
-        enabled: !0
-      },
-      zoom: {
-        enabled: false
-      }
+        height: 115,
+        type: 'area',
+        sparkline: {
+            enabled: !0
+        },
+        zoom: {
+            enabled: false
+        }
     },
     dataLabels: {
-      enabled: false
+        enabled: false
     },
     stroke: {
-      width: 1.7,
-      curve: 'smooth'
+        width: 1.7,
+        curve: 'smooth'
     },
     fill: {
-      type: 'gradient',
-      gradient: {
-        shade: 'dark',
-        gradientToColors: ['#6610f2'],
-        shadeIntensity: 1,
-        type: 'vertical',
-        opacityFrom: 0.5,
-        opacityTo: 0.0,
-        //stops: [0, 100, 100, 100]
-      },
+        type: 'gradient',
+        gradient: {
+            shade: 'dark',
+            gradientToColors: ['#6610f2'],
+            shadeIntensity: 1,
+            type: 'vertical',
+            opacityFrom: 0.5,
+            opacityTo: 0.0,
+        },
     },
-
     colors: ["#6610f2"],
     tooltip: {
-      theme: "dark",
-      fixed: {
-        enabled: !1
-      },
-      x: {
-        show: !1
-      },
-      y: {
-        title: {
-          formatter: function (e) {
-            return ""
-          }
+        theme: "dark",
+        fixed: {
+            enabled: !1
+        },
+        x: {
+            show: !1
+        },
+        y: {
+            title: {
+                formatter: function (e) {
+                    return ""
+                }
+            }
+        },
+        marker: {
+            show: !1
         }
-      },
-      marker: {
-        show: !1
-      }
     },
     xaxis: {
-      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
     }
-  };
+};
 
-  var chart = new ApexCharts(document.querySelector("#chart5"), options);
-  chart.render();
-
-
-
-
+var chart = new ApexCharts(document.querySelector("#chart5"), options);
+chart.render();
 
 });
